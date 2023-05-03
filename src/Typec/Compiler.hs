9a27b8282051e7550055cd5b347e58f7218690b7
vglfr <vf.velt@gmail.com>
2023-05-03 19:58:40 +0300

    Translate AST to VM instructions

--- src/Typec/Compiler.hs
+++ src/Typec/Compiler.hs
@@ -1,8 +1,12 @@
 module Typec.Compiler where
 
-import System.Process (readProcess)
-import Typec.AST (Exp, Prog)
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
 
+-- import System.Process (readProcess)
+import Typec.AST (Exp ((:+), (:-), (:*), (:/), Val))
+
+{-
 declare :: String
 declare = unlines
   [
@@ -70,9 +74,6 @@ offset = ("        " <>)
 compile :: Prog -> String
 compile _ = unlines [declare, global, main]
 
-spool :: Exp -> [Exp]
-spool = undefined
-
 run :: String -> IO ()
 run s = do
   putStrLn s
@@ -80,3 +81,58 @@ run s = do
   readProcess "nasm" ["-f", "elf64", "-o", "/tmp/temp.o", "/tmp/temp.s"] mempty >>= putStrLn
   readProcess "gcc" ["-z", "noexecstack", "-o", "/tmp/temp", "-lc", "/tmp/temp.o"] mempty >>= putStrLn
   readProcess "/tmp/temp" mempty mempty >>= putStrLn
+-}
+
+data Ins
+  = Val :+: Val
+  | Val :-: Val
+  | Val :*: Val
+  | Val :/: Val
+
+data Val
+  = Imm Double
+  | Ref Int
+
+instance Show Ins where
+  show (a :+: b) = show a <> " + " <> show b
+  show (a :-: b) = show a <> " - " <> show b
+  show (a :*: b) = show a <> " * " <> show b
+  show (a :/: b) = show a <> " / " <> show b
+
+instance Show Val where
+  show (Imm x) = show x
+  show (Ref x) = "[" <> show x <> "]"
+
+spool :: Exp -> [Ins]
+spool t = let es = flat t
+           in fmap (ref es) es
+ where
+  flat e = case e of
+             a :+ b -> flat a <> flat b <> [e]
+             a :- b -> flat a <> flat b <> [e]
+             a :* b -> flat a <> flat b <> [e]
+             a :/ b -> flat a <> flat b <> [e]
+             _ -> mempty
+  ref es e = case e of
+               a :+ b -> val a es e :+: val b es e
+               a :- b -> val a es e :-: val b es e
+               a :* b -> val a es e :*: val b es e
+               a :/ b -> val a es e :/: val b es e
+               _ -> undefined
+  val e es e' = case e of
+                  Val x -> Imm x
+                  _ -> Ref $ index e es - index e' es 
+  index e es = fromJust $ elemIndex e es
+
+compile :: [Ins] -> String
+compile = undefined
+ where
+  compile' :: Ins -> String
+  compile' i = case i of
+                 a :+: b -> undefined
+                 a :-: b -> undefined
+                 a :*: b -> undefined
+                 a :/: b -> undefined
+  imm i = case i of
+            Imm x -> undefined
+            Ref x -> undefined

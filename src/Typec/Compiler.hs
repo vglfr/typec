5e5a5b8ed19b7be6ee63a8c2045d8c945b26f3ae
vglfr <vf.velt@gmail.com>
2023-05-04 18:58:12 +0300

    Untested implementation of binop compiler

--- src/Typec/Compiler.hs
+++ src/Typec/Compiler.hs
@@ -1,10 +1,14 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Typec.Compiler where
+
+import Prelude hiding (div)
 
 import Data.List (elemIndex)
 import Data.Maybe (fromJust)
 
 -- import System.Process (readProcess)
-import Typec.AST (Exp (Bin, Val), Op)
+import Typec.AST (Exp (Bin, Val), Op (Add, Div, Mul, Sub))
 
 {-
 declare :: String
@@ -68,9 +72,6 @@ exit = unlines . fmap offset $
   , "syscall"
   ]
 
-offset :: String -> String
-offset = ("        " <>)
-
 compile :: Prog -> String
 compile _ = unlines [declare, global, main]
 
@@ -85,10 +86,6 @@ run s = do
 
 data Ins
   = Two Op Val Val
-  -- = Val :+: Val
-  -- | Val :-: Val
-  -- | Val :*: Val
-  -- | Val :/: Val
 
 data Val
   = Imm Double
@@ -96,14 +93,13 @@ data Val
 
 instance Show Ins where
   show (Two o a b) = show a <> show o <> show b
-  -- show (a :+: b) = show a <> " + " <> show b
-  -- show (a :-: b) = show a <> " - " <> show b
-  -- show (a :*: b) = show a <> " * " <> show b
-  -- show (a :/: b) = show a <> " / " <> show b
 
 instance Show Val where
   show (Imm x) = show x
   show (Ref x) = "[" <> show x <> "]"
+
+offset :: String -> String
+offset = ("        " <>)
 
 spool :: Exp -> [Ins]
 spool t = let es = flat t
@@ -111,33 +107,71 @@ spool t = let es = flat t
  where
   flat e = case e of
              Bin _ a b -> flat a <> flat b <> [e]
-             -- a :+ b -> flat a <> flat b <> [e]
-             -- a :- b -> flat a <> flat b <> [e]
-             -- a :* b -> flat a <> flat b <> [e]
-             -- a :/ b -> flat a <> flat b <> [e]
              _ -> mempty
   ref es e = case e of
                Bin o a b -> Two o (val a es e) (val b es e)
-               -- a :+ b -> val a es e :+: val b es e
-               -- a :- b -> val a es e :-: val b es e
-               -- a :* b -> val a es e :*: val b es e
-               -- a :/ b -> val a es e :/: val b es e
                _ -> undefined
   val e es e' = case e of
                   Val x -> Imm x
                   _ -> Ref $ index e es - index e' es 
   index e es = fromJust $ elemIndex e es
 
--- compile :: [Ins] -> String
--- compile = unlines . fmap compile'
---  where
---   compile' :: Ins -> String
---   compile' i = case i of
---                  _a :+: _b -> undefined
---                  _a :-: _b -> undefined
---                  _a :*: _b -> undefined
---                  _a :/: _b -> undefined
+compile :: [Ins] -> String
+compile = unlines . fmap ins
 
-  -- val i = case i of
-  --           Imm _x -> undefined
-  --           Ref _x -> undefined
+ins :: Ins -> String
+ins i@(Two o a b) = unlines . fmap offset $
+     comm i
+  <> stage a b
+  <> invoke o b
+  <> push
+
+comm :: Ins -> [String]
+comm i = pure $ "; " <> show i
+
+stage :: Val -> Val -> [String]
+stage a b = case a of
+              Imm _ -> case b of
+                         Imm _ -> mov "rax" a
+                         Ref _ -> mov "rax" a <> pop "rbx"
+              Ref r -> case b of
+                         Imm _ -> pop "rax"
+                         Ref r' -> if r > r'
+                                   then pop "rax" <> pop "rbx"
+                                   else pop "rbx" <> pop "rax"
+
+invoke :: Op -> Val -> [String]
+invoke o v = case o of
+               Add -> add v
+               Sub -> sub v
+               Mul -> mul v
+               Div -> div v
+
+mov :: String -> Val -> [String]
+mov r (Imm v) = pure $ "mov         " <> r <> ", " <> show v
+
+pop :: String -> [String]
+pop r = pure $ "pop         " <> r
+
+add :: Val -> [String]
+add v = pure $ case v of
+                 Imm i -> "add         rax, " <> show i
+                 Ref _ -> "add         rax, rbx"
+
+sub :: Val -> [String]
+sub v = pure $ case v of
+                 Imm i -> "sub         rax, " <> show i
+                 Ref _ -> "sub         rax, rbx"
+
+mul :: Val -> [String]
+mul v = case v of
+          Imm i -> ["mul         rax, " <> show i, "cqo"]
+          Ref _ -> ["mul         rax, rbx"       , "cqo"]
+
+div :: Val -> [String]
+div v = case v of
+          Imm i -> ["div         rax, " <> show i, "cqo"]
+          Ref _ -> ["div         rax, rbx"       , "cqo"]
+
+push :: [String]
+push = pure $ "push        rax"
